3efe9bd55e0e77787bf5d6e2a2386f97c5076493
Krishna <anonymous@google.com>
2025-08-01 12:16:18 +0530

    Deleted some temporary files and Completed Linked Queue

--- Queue/Linked_queue.c
+++ Queue/Linked_queue.c
@@ -3,13 +3,83 @@
 
 typedef struct Node {
     int data;
-    node *next;
+    struct Node *next;
 } node;
 
-node *createNode(int val) {
-    
-}
-int main() {
+node *front = NULL; // Front = Tail;
+node *rear = NULL;  // Rear = Head;
 
+node *createNode(int val) {
+    node *n = (node*)malloc(sizeof(node));
+    n->data = val;
+    n->next= NULL;
+    return n;
+}
+
+void enqueue(int val){
+    node *newNode = createNode(val);
+    if(newNode==NULL){
+        printf("\nThe QUEUE is full.\n");
+    }
+    else{
+        if(front==NULL && rear == NULL){
+            front= newNode;
+            rear= newNode;
+        }
+        else{
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+}
+
+
+
+int dequeue(){
+    if(front==NULL){
+        printf("\nThe QUEUE is empty\n");
+        return -1;
+    }
+    else{
+        int val = front->data;
+        node *prev = front;
+        front = front->next;
+        free(prev);
+        return val;
+    }
+}
+
+// Display front ---> rear
+void display(){
+    node *n;
+    if(front==NULL){
+        printf("\nThe QUEUE is empty\n");
+    }
+    else{
+        n = front;
+        while(n!=NULL){
+            printf("%d\t",n->data);
+            n= n->next;
+        }
+    }
+}
+
+int peek(){
+    node *n;
+    if(front==NULL){
+        printf("\nThe QUEUE is empty\n");
+    }
+    else{
+        return rear->data;
+    }
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    dequeue();
+    display();
     return 0;
 }
