77b32da9b16e70c21f4c695d2479e3dc60a85c4a
Manish <mks25022025@gmail.com>
2025-08-15 22:02:27 +0530

    adding deleting function

--- Trees/BST_Operations.c
+++ Trees/BST_Operations.c
@@ -1,152 +1,144 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node {
+typedef struct Node
+{
     struct Node *left;
     int data;
     struct Node *right;
 } TreeNode;
 
-TreeNode *createNode(int val){
-    TreeNode *newNode = (TreeNode*)malloc(sizeof(TreeNode));
+TreeNode *createNode(int val)
+{
+    TreeNode *newNode = (TreeNode *)malloc(sizeof(TreeNode));
     newNode->left = NULL;
     newNode->right = NULL;
     newNode->data = val;
     return newNode;
 }
 
-TreeNode* Insertion(TreeNode *root ,int val){
-    TreeNode* prev;
-    TreeNode* n;
+TreeNode *Insertion(TreeNode *root, int val)
+{
+    TreeNode *prev;
+    TreeNode *n;
     TreeNode *newNode = createNode(val);
-    if(root==NULL){
+    if (root == NULL)
+    {
         printf("The BST is NULL\n");
         return newNode;
     }
-    else{
+    else
+    {
         n = root;
-        while(n!=NULL){
+        while (n != NULL)
+        {
             prev = n;
-            if(val==n->data){
+            if (val == n->data)
+            {
                 printf("The Node already exists.\n");
                 return root;
             }
-            else if(val>n->data){
-                n= n->right;
+            else if (val > n->data)
+            {
+                n = n->right;
             }
-            else{
+            else
+            {
                 n = n->left;
             }
         }
 
-        if(val>prev->data){
+        if (val > prev->data)
+        {
             prev->right = newNode;
         }
-        else{
+        else
+        {
             prev->left = newNode;
         }
         return root;
     }
 }
 
-
-TreeNode* findMin(TreeNode *root){
+TreeNode *findMin(TreeNode *root)
+{
     TreeNode *n = root;
-    while(n->right!=NULL){
+    while (n->right != NULL)
+    {
         n = n->right;
     }
     return n;
 }
 
-// Incomplete
-TreeNode* Deletion(TreeNode *root,int val){
-    TreeNode *prev;
-    TreeNode *n;
-    TreeNode *rightNode;
-    if(root==NULL){
-        printf("The BST is NULL");
-        return NULL;
+// Function to delete a node from the BST
+TreeNode *inOrderPredecessor(TreeNode *node)
+{
+    node = node->left;
+    while (node->right != NULL)
+    {
+        node = node->right;
     }
-    else{
-        n = root;
-        prev = n;
-        while(n!=NULL){
-            if(n->data==val){
-                break;
-            }
-            else if(val>n->data){
-                prev = n;
-                n= n->right;
-            }
-            else{
-                prev = n;
-                n= n->left;
-            }
-        }
-        if(n==NULL){
-            printf("Node not found");
-            return root;
-        }
-        else if(n->right==NULL && n->left == NULL){
-            if(prev->left == n){
-                prev->left = n->left;
-            }
-            else{
-                prev->right = n->right;
-            }
-            free(n);
-            return root;
-        }
-        else if(n->left != NULL && prev->left == n){
-            prev->left = n->left;
-            return root;
-        }
-        else if(n->right != NULL && prev->right == n){
-            prev->left = n->left;
-            return root;
-        }
-        else{
-            rightNode = findMin(n->left);
-            rightNode->right = n->right;
-            if(n==root){
-                root= n->left;
-            }
-            else{
-                if(prev->left==n){
-                    prev->left=rightNode;
-                }
-                else{
-                    prev->right=rightNode;
-                }
-            }
-            free(n);
-            return root;
-        }
-    }
+    return node;
 }
 
+TreeNode *deleteNode(TreeNode *node, int key)
+{
+    TreeNode *iPre = NULL;
 
+    if (node == NULL)
+    {
+        printf("The value is not exist in the tree.\n");
+        return NULL;
+    }
+    if (node->left == NULL && node->right == NULL)
+    {
+        if (node->data == key)
+        {
+            free(node);
+            return NULL;
+        }
+    }
 
+    if (key < node->data)
+    {
+        node->left = deleteNode(node->left, key);
+    }
+    else if (key > node->data)
+    {
+        node->right = deleteNode(node->right, key);
+    }
+    else
+    {
+        iPre = inOrderPredecessor(node);
+        node->data = iPre->data;
+        node->left = deleteNode(node->left, iPre->data);
+    }
+    return node;
+}
 
-void InOrder(TreeNode *root){
-    if(root!=NULL){
+void InOrder(TreeNode *root)
+{
+    if (root != NULL)
+    {
         InOrder(root->left);
-        printf("%d\t",root->data);
+        printf("%d\t", root->data);
         InOrder(root->right);
     }
 }
+
 int main()
 {
-/*
-          9
+    /*
+              9
+             / \
+            8  11
+           /  /  \
+          6  10   14
          / \
-        8  11
-       /  /  \
-      6  10   14
-     / \ 
-    5   7
-*/
+        5   7
+    */
 
+    // Creating the BST
     TreeNode *root = createNode(9);
 
     TreeNode *p1 = createNode(8);
@@ -155,27 +147,27 @@ int main()
     TreeNode *d2 = createNode(7);
 
     root->left = p1;
-    p1->left= c1;
-    c1->left=d1;
-    c1->right=d2;
-
+    p1->left = c1;
+    c1->left = d1;
+    c1->right = d2;
 
     TreeNode *p2 = createNode(11);
     TreeNode *e1 = createNode(10);
     TreeNode *e2 = createNode(14);
-    
+
     root->right = p2;
     p2->left = e1;
     p2->right = e2;
 
-    root = Insertion(root,4);
-
-    root = Deletion(root,11);
-    printf("\n InOrder Traversal of BST :\n");
+    // Operations on the BST
+    printf("\n Traversal before opration: \n");
     InOrder(root);
-    // printf("\n%d\n",find_right(root)->data);
 
+    // root = Insertion(root,24);
+    root = deleteNode(root, 8);
 
-    
+    printf("\n Traversal after insertion: \n");
+    InOrder(root);
+
     return 0;
 }
